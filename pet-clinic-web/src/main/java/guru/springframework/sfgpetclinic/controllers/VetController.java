4412e8080e921c6ec6d4d98f372e4c0e595a04b4
oleksandr.kydiuk <okydiuk@timetrade.com>
2019-02-08 16:52:09 +0200

    adding Owner and Vet List

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,16 +1,27 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by oleksandr.kydiuk on Feb, 2019
  */
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.index"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/index", "/index.index"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
