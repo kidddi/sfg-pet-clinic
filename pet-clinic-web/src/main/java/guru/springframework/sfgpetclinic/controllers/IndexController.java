d6ae4604eabac007252a87d2ab491d08dd40bded
oleksandr.kydiuk <okydiuk@timetrade.com>
2019-02-08 13:29:00 +0200

    adding index page and controller

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by oleksandr.kydiuk on Feb, 2019
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
