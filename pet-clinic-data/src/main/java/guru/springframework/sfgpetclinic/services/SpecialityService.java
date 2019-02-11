93b9b64410a597a7c8e4fa0996b1bf246dcf6d5d
oleksandr.kydiuk <kidddi@ukr.net>
2019-02-11 17:47:54 +0200

    adding Specialities to bootstrap

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by oleksandr.kydiuk on Feb, 2019
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
